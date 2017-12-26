eedc26bceaaf62b313c118c42c1ac57c033181e5
CodeArtha <codeartha@gmail.com>
2017-12-26 23:11:20 +0100

    just an example ES6 style js class for memo

---
+++ ES6classexample.js
@@ -0,0 +1,18 @@
+// example of new class declaration complying ECMA script 6
+class ClassName extends OtherClass{
+	// special constructor function for class instanciation.
+	constructor(param1, param2){
+		this._paramA = param1;
+		this._param2 = param2 // one can now end line with a linefeed, no more ; needed.
+	}
+
+	//regular function
+	genRandomBoard(param){
+		return param
+	}
+
+	genSymetricBoard(){
+
+	}
+
+}
